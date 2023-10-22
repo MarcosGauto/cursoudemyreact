5e87c018345abfe7dd5855d2ddcf37726943e8a3
Chipi87 <mgauto@outlook.es>
2023-10-22 19:28:57 -0300

    curso

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Formulario() {
+  return (
+    <div className='md:w-1/2 lg:2/5'>
+        <h2 className='font-black text-3xl text-center'>Seguimientos de pacientes</h2>
+          <p className='text-lg mt-5 text-center'>
+            Añade pacientes y {" "}
+            <span className='text-indigo-600 font-bold'>Administralos</span>
+          </p>
+          <form>
+            
+          </form>
+      
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ListadoPacientes() {
+  return (
+    <div className='w-1/2 lg-3/5'>
+        <h2>LIstado de Pacientes </h2>
+      
+    </div>
+  )
+}
+
+export default ListadoPacientes
